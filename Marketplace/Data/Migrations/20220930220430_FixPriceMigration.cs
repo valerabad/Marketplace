606e9a66c9ef6055f34c9e4022821e925b620a22
Valeriy Baditsa <valerabaditsa@gmail.com>
2022-10-02 13:12:34 +0300

    Add migrations

---
+++ Marketplace/Data/Migrations/20220930220430_FixPriceMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Marketplace.Data.Migrations
+{
+    public partial class FixPriceMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Sales",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Sales",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+        }
+    }
+}
