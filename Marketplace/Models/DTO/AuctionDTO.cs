101d98762969a40a5ef8f50f81f4f00c3b395fea
Valerii Baditsa <valerabaditsa@gmail.com>
2022-10-05 15:36:40 +0300

    Update AuctionDTO.cs
    Fix

--- Marketplace/Models/DTO/AuctionDTO.cs
+++ Marketplace/Models/DTO/AuctionDTO.cs
@@ -1,19 +1,19 @@
+using System;
 using Marketplace.Common;
 
 namespace Marketplace.Models.DTO;
 
-public class AuctionDTO
+public class AuctionDto
 {
-    public int ItemId { get; set; }
-    
-    public int SaleId { get; set; }
-    
+    public int Id { get; set; }
     public string Name { get; set; }
     
     public MarketStatus Status { get; set; }
     
     public decimal Price { get; set; }
+    
+    public string Seller { get; set; }
 
-    public string CreatedDt { get; set; }
+    public DateTime CreatedDt { get; set; }
     
 }
